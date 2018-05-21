cb8c415d5bdb8bf7420dbd35399509a1db1c440b
Hayes <xbzhs@163.com>
2018-05-21 20:30:34 +0800

    add

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="header">
-        <div class="header-left"><span class="iconfont">&#xe624;</span></div>
+        <div class="header-left"><span class="iconfont back-icon">&#xe624;</span></div>
         <div class="header-input"><div class="iconfont">&#xe632;</div>输入城市/酒店/游玩主题</div>
-        <div class="header-right">城市<span class="iconfont">&#xe64a;</span></div>
+        <div class="header-right">城市<span class="iconfont arrow-icon">&#xe64a;</span></div>
     </div>
 </template>
 <script>
@@ -11,25 +11,34 @@ export default{
 }
 </script>
 <style lang="stylus" scoped>
-    .header
-      display:flex
-      line-height: .86rem
-      background:#00bcd4
-      color:#fff
-      .header-left
-        width:.64rem
-        float:left
-      .header-input
-         flex:1 
-         margin-top:.12rem
-         height:.64rem
-         line-height:.64rem
-         margin-left:.2rem
-         background:#fff
-         border-radius:.1rem
-         color:#ccc
-      .header-right
-        width:1.24rem
-        float:right
-        text-align:center
+  .header
+    display: flex
+    line-height: .86rem
+    background:#00bcd4
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      min-width: 1.04rem
+      padding: 0 .1rem
+      float: right
+      text-align: center
+      color: #fff
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 </style>
