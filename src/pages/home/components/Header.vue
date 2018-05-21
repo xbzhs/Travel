690ddd460eddd1aade06f3d3ce02978e607c9f24
Hayes <xbzhs@163.com>
2018-05-21 20:18:36 +0800

    add

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        <div class="header-left"><span class="iconfont">&#xe624;</span></div>
+        <div class="header-input"><span class="iconfont">&#xe632;</span>输入城市/酒店/游玩/主题</div>
+        <div class="header-right">城市</div>
+    </div>
+</template>
+<script>
+export default{
+    name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+    .header
+      display:flex
+      line-height: .86rem
+      background:#00bcd4
+      color:#fff
+      .header-left
+        width:.64rem
+        float:left
+      .header-input
+         flex:1 
+         margin-top:.12rem
+         height:.64rem
+         line-height:.64rem
+         margin-left:.2rem
+         background:#fff
+         border-radius:.1rem
+         color:#ccc
+      .header-right
+        width:1.24rem
+        float:right
+        text-align:center
+</style>
